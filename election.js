397cca85ec93cf1a6183551a199aa6903438fa80
Ndelforno <nicolas.delforno@gmail.com>
2018-10-31 14:46:22 -0400

    exercise done

--- election.js
+++ election.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   // Imagination!
+  var candidatesList = document.getElementById('candidatesList');
+
+  var request = axios({
+      url:'https://bb-election-api.herokuapp.com/',
+      method: 'get',
+      data: '',
+      dataType: 'JSON'
+    }).then(function(response){
+      var candidates = response.data.candidates
+      candidates.forEach(function(candidate){
+        var infos = document.createElement('li')
+        infos.innerText = candidate.name + ', Votes: ' + candidate.votes
+        candidatesList.append(infos);
+      })
+    })
 
 });
